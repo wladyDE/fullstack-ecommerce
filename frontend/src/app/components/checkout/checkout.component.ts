ecb578087ac2daff792f09484494be9d7214f659
wladyH <volodymyr.havryliuk@dataciders.com>
2025-01-16 11:17:14 +0100

    add checkout form layout

---
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,68 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  standalone: true,
+  imports: [ReactiveFormsModule, CurrencyPipe],
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.scss'
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup!: FormGroup
+
+  totalPrice: number = 0
+  totalQuantity: number = 0
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
+      })
+    })
+  }
+
+  onSubmit(){
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+
+  copyShippingAddressToBillingAddress(event: Event): void {
+    const checkbox = event.target as HTMLInputElement;
+
+    if (checkbox.checked) {
+      this.checkoutFormGroup.get('billingAddress')?.setValue(
+        this.checkoutFormGroup.get('shippingAddress')?.value
+      );
+    } else {
+      this.checkoutFormGroup.get('billingAddress')?.reset();
+    }
+  }
+}
