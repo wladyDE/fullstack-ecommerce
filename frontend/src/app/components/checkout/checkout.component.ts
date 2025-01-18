7e82bf34662bb67324984d9c16d2faab600783ea
wladyH <volodymyr.havryliuk@dataciders.com>
2025-01-18 10:30:15 +0100

    add validation for Billing Address

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -74,11 +74,26 @@ export class CheckoutComponent implements OnInit {
           ]),
       }),
       billingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: [''],
+        street: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace
+          ]),
+        city: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace
+          ]),
+        state: new FormControl('', [ Validators.required ]),
+        country: new FormControl('', [ Validators.required ]),
+        zipCode: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace
+          ]),
       }),
       creditCard: this.formBuilder.group({
         cardType: [''],
@@ -126,6 +141,12 @@ export class CheckoutComponent implements OnInit {
   get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state') }
   get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country') }
   get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode') }
+
+  get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street') }
+  get billingAddressCity() { return this.checkoutFormGroup.get('billingAddress.city') }
+  get billingAddressState() { return this.checkoutFormGroup.get('billingAddress.state') }
+  get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country') }
+  get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode') }
 
   copyShippingAddressToBillingAddress(event: Event): void {
     const checkbox = event.target as HTMLInputElement;
